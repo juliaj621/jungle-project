78bb09e38197ac19d4987abf16ebb8b9e81a7a01
Julia Jachimowicz <juliaj621@gmail.com>
2019-11-14 12:02:42 -0800

    added routes for users

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,10 @@ Rails.application.routes.draw do
 
   get '/about', :to => 'about#show' 
 
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+  
+
   resource :cart, only: [:show] do
     post   :add_item
     post   :remove_item
