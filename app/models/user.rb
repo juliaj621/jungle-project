26815e9bd1f79c39af3a4dae6f76463959dffd5a
Julia Jachimowicz <juliaj621@gmail.com>
2019-11-19 12:09:36 -0800

    implemented authenticate method

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,10 @@ class User < ActiveRecord::Base
   validates :first_name, presence: true
   validates  :last_name, presence: true
   
+  def self.authenticate_with_credentials(email, password)
+    email = email.downcase.strip
+    user = User.find_by_email(params[:email])
+    # If the user exists AND the password entered is correct.
+    if user && user.authenticate(params[:password])
+  end
 end
