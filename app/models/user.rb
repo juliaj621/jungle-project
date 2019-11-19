f4d1ca21f4a2a69aa9eb31d9c132db485a0da55d
Julia Jachimowicz <juliaj621@gmail.com>
2019-11-19 13:05:59 -0800

    updated validations and implemented tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ActiveRecord::Base
 
   validates :email, presence: true, uniqueness: true
   validates :password, presence: true, length: { minimum: 5 }
+  validates :password_confirmation, presence: true, length: { minimum: 5 }
   validates :first_name, presence: true
   validates  :last_name, presence: true
 
--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -2,21 +2,44 @@ require "rails_helper"
 
 RSpec.describe User, type: :model do
 
-  describe 'Validations' do
-    # validation examples here
 
+  describe 'Validations' do
     # These are required when creating the model so you should also have an example for this
+    it "should be valid with all fields correct" do
+      @user = User.new(first_name: "Julia", last_name: "J", email: "pineapple@pineapple.com", password: "password", password_confirmation: "password")
+      expect(@user).to be_valid
+    end
+   
     it "should have a password and password_confirmation input" do
-      expect(fields).to be_valid
+      @user = User.new(first_name: "Julia", last_name: "J", email: "pineapple@pineapple.com", password: "password", password_confirmation: nil)
+      expect(@user).not_to be_valid
     end
 
     # These need to match so you should have an example for where they are not the same
-    it "should have a password and password_confirmation that match" do
-      expect(password).to match(password_confirmation)
-    end
     it "should not be valid if password and password_confirmation do not match" do
-      expect(password).not_to match(password_confirmation)
+      @user = User.new(first_name: "Julia", last_name: "J", email: "pineapple@pineapple.com", password: "password", password_confirmation: "passwor")
+      expect(@user).not_to be_valid
     end
+
+    # Emails must be unique (not case sensitive)
+    it "should not be valid if email already exists in db" do
+      @user1 = User.create(first_name: "Julia", last_name: "J", email: "pineapple@pineapple.com", password: "password", password_confirmation: "password")
+      @user2 = User.new(first_name: "Julia", last_name: "J", email: "pineapple@pineapple.com", password: "password", password_confirmation: "password")
+      expect(@user2).not_to be_valid
+    end
+
+    # Email, first name, and last name should also be required
+    it "should not be valid without email, first name, and last name" do
+      @user = User.new(first_name: nil, last_name: nil, email: nil, password: "password", password_confirmation: "password")
+      expect(@user).not_to be_valid
+    end
+
+    # The password must have a minimum length when a user account is being created.
+    it "should not be valid with password of length less than 5" do
+      @user = User.new(first_name: "Julia", last_name: "J", email: "pineapple@pineapple.com", password: "hi", password_confirmation: "hi")
+      expect(@user).not_to be_valid
+    end
+
   end
 
   describe '.authenticate_with_credentials' do
