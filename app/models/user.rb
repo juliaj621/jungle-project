446a05b44f0cf46e90cc395dfada9cdd78f2e18d
Julia Jachimowicz <juliaj621@gmail.com>
2019-11-19 12:16:26 -0800

    fixed authenticate credentials method

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,11 +6,12 @@ class User < ActiveRecord::Base
   validates :password, presence: true, length: { minimum: 5 }
   validates :first_name, presence: true
   validates  :last_name, presence: true
-  
+
   def self.authenticate_with_credentials(email, password)
     email = email.downcase.strip
-    user = User.find_by_email(params[:email])
+    user = User.find_by_email(email)
     # If the user exists AND the password entered is correct.
-    if user && user.authenticate(params[:password])
+    user.authenticate(password)
+
   end
 end
