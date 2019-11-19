10e49dfc026c537a546c2ec1142e02d95e32cd59
Julia Jachimowicz <juliaj621@gmail.com>
2019-11-19 12:09:00 -0800

    added validations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,10 @@
 class User < ActiveRecord::Base
 
   has_secure_password
+
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true, length: { minimum: 5 }
+  validates :first_name, presence: true
+  validates  :last_name, presence: true
   
 end
